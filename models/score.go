0af0203e78aae6ba6ffe197d418b47e0ffd46b1e
linyu <952637635@qq.com>
2021-09-09 00:01:47 +0800

    继续完善

--- models/score.go
+++ models/score.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"go-cpulimiter/pkg/config"
-	"log"
 )
 
 type Score struct {
@@ -23,10 +22,10 @@ func (score *Score) ChangeScore(vmid uint, plusor string) {
 
 	if plusor == "-" {
 		scoreData.Score--
-		log.Printf("已更新VMID %d，扣分，目前：%d", vmid, scoreData.Score)
+		//log.Printf("已更新VMID %d，扣分，目前：%d", vmid, scoreData.Score)
 	} else {
 		scoreData.Score++
-		log.Printf("已更新VMID %d，加分，目前：%d", vmid, scoreData.Score)
+		//log.Printf("已更新VMID %d，加分，目前：%d", vmid, scoreData.Score)
 	}
 
 	DB.Model(score).Where("vps_id = ?", vmid).Update("score", scoreData.Score)
